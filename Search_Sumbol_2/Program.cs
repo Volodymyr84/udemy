2de0b737a9d03468226f8f3b3b9628cd528c4ea5
Volodymyr84 <38746456+Volodymyr84@users.noreply.github.com>
2019-02-23 15:52:57 +0200

    tes

--- Search_Sumbol_2/Program.cs
+++ Search_Sumbol_2/Program.cs
@@ -19,6 +19,22 @@ namespace Search_Sumbol_2
              Random rnd = new Random();
             int J = rnd.Next(0, mus);
 
+            // int x = rnd.Next(Console.WindowHeight, Console.WindowWidth);
+            //int y = rnd.Next(Console.WindowHeight, Console.WindowWidth);
+
+            int rand = rnd.Next(0, 15);
+            for (int k = 0; k <rand; k++)
+            {
+                //int x = 9;
+                //int y = 10;
+                int x = rnd.Next(Console.WindowHeight, Console.WindowWidth);
+                int y = rnd.Next(Console.WindowHeight, Console.WindowWidth);
+
+                Console.SetCursorPosition(x, y);
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write('C');
+                rand = rnd.Next(0, 15);
+            }
 
             Console.ReadKey();
                         
