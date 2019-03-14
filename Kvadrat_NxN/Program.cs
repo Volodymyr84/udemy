b5ad422a300ec62ff7a2aafb042ef7aa3f329227
Volodymyr84 <38746456+Volodymyr84@users.noreply.github.com>
2019-03-14 10:26:42 +0200

    Kvadrat N*N

---
+++ Kvadrat_NxN/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Kvadrat_NxN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = Convert.ToInt32(Console.ReadLine());
+            if (1 <= N & N <= 80)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        Console.Write('*');
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
