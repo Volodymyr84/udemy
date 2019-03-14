1acfdd7411a7a1fbf4d96044709b66a49369c06a
Volodymyr84 <38746456+Volodymyr84@users.noreply.github.com>
2019-03-14 14:26:22 +0200

    Separator

---
+++ Separator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Separator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] separators = { ",", ".", "!", "?", ";", ":", " ", "/", "'", "|" };
+            string[] a = Console.ReadLine().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            int count = 1;
+            for (int i = 0; i < a.Length; i++)
+            {
+                for (int j = 1; j < a.Length; j++)
+                {
+                    if (a[i] == a[j])
+                    {
+                        count++;
+                    }
+                }
+                count = 1;
+                Console.WriteLine(a[i] +"-"+ count);
+            }
+            
+        }
+    }
+}
+
+
+
+
+/*
+ * Дан текст телеграммы, введенный в одну строку. Слова в тексте разделяются одним или несколькими пробелами. Текст может содержать знаки препинания:
+    , . : ; ? ! - " '
+    Требуется посчитать, какие слова есть в этом тексте и сколько раз они встречаются. Слова вывести в алфавитном порядке в нижнем регистре в форме
+    слово - КоличествоПовторений
+ */
